e68890a6e0728a0bec78d280a10525f670ae138f
Henny20 <47795195+Henny20@users.noreply.github.com>
2024-07-16 23:48:28 +0200

    Add files via upload

---
+++ AgConn.CrossPlatform/Views/CommSettingsWindow.axaml.cs
@@ -0,0 +1,21 @@
+﻿using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace AgConn.CrossPlatform.Views;
+
+public partial class CommSettingsWindow : Window
+{
+    public CommSettingsWindow()
+    {
+        InitializeComponent();
+#if DEBUG
+        this.AttachDevTools();
+#endif
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
+
---
+++ AgConn.CrossPlatform/Views/MainWindow.axaml.cs
@@ -0,0 +1,14 @@
+using Avalonia.Controls;
+
+namespace AgConn.CrossPlatform.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+       // this.Closing += OnClosing;
+    }
+    
+
+}
