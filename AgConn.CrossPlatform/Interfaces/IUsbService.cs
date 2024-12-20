009061ee202ef0490aaf1ac54bd18f600e1595a1
Henny20 <47795195+Henny20@users.noreply.github.com>
2024-12-20 21:48:04 +0100

    Add files via upload

--- AgConn.CrossPlatform/Interfaces/IUsbService.cs
+++ AgConn.CrossPlatform/Interfaces/IUsbService.cs
@@ -1,1 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AgConn.CrossPlatform.Interfaces;
+
+public interface IUsbService
+{
+
+    public string[] GetPortNames();
+    // public Task<string[]> GetPortNames();
+}
+
 
